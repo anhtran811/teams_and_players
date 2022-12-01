fe5b64853638dfd26673e6fa58debe7bd12551c8
Anhnhi Tran <anhtran811@gmail.com>
2022-12-01 16:27:55 -0700

    User story 5 complete with formatting to player views

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/teams/:id', to: 'teams#show'
   get '/player_table_name', to: 'players#index'
   get '/player_table_name/:id', to: 'players#show'
+  get '/teams/:team_id/player_table_name', to: 'team_players#index'
 end
---
+++ spec/features/teams/players/index_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+# As a visitor
+# When I visit '/parents/:parent_id/child_table_name'
+# Then I see each Child that is associated with that Parent with each Child's attributes
+# (data from each column that is on the child table)
+
+RSpec.describe 'Teams Players Index' do
+  describe 'As a visitor' do
+    it 'can visit /teams/:team_id/player_table_name' do
+      spain = Team.create!(name: 'Spain', rank: 7, qualified: true)
+      spain.players.create!(name: 'Gavi', age: 18, old_enough: false)
+
+      visit "/teams/#{spain.id}/player_table_name"
+
+      expect(current_path).to eq("/teams/#{spain.id}/player_table_name")
+    end
+    
+    it 'can see each Player that is associated with that Team and each Players attributes' do
+      spain = Team.create!(name: 'Spain', rank: 7, qualified: true)
+      gavi = spain.players.create!(name: 'Gavi', age: 18, old_enough: false)
+      torres = spain.players.create!(name: 'Torres', age: 25, old_enough: true)
+
+      visit "/teams/#{spain.id}/player_table_name"
+
+      expect(page).to have_content(spain.name)
+      expect(page).to have_content(gavi.name)
+      expect(page).to have_content(gavi.age)
+      expect(page).to have_content(gavi.old_enough)
+      expect(page).to have_content(torres.name)
+      expect(page).to have_content(torres.age)
+      expect(page).to have_content(torres.old_enough)
+    end
+
+  end
+end
