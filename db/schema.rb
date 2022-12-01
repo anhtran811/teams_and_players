15cbf43b1271e56e1fda2eb881173bc49e2fff0b
Anhnhi Tran <anhtran811@gmail.com>
2022-12-01 16:23:49 -0700

    Added migration of teams and players, schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_01_000821) do
+ActiveRecord::Schema.define(version: 2022_12_01_183023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2022_12_01_000821) do
     t.boolean "old_enough"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "team_id"
+    t.index ["team_id"], name: "index_players_on_team_id"
   end
 
   create_table "teams", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 2022_12_01_000821) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "players", "teams"
 end
