62b79f2e00d02b8c6bb0d8cd1e8d17ac1d89da28
Anhnhi Tran <anhtran811@gmail.com>
2022-11-30 20:20:08 -0700

    User story 3: created associated files, folders, and database for  players. Completed index route and view.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_29_220145) do
+ActiveRecord::Schema.define(version: 2022_12_01_000821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.boolean "old_enough"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
