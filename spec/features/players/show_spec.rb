2a6539254ddbb831281076e31f56b742b7bfe7a7
Anhnhi Tran <anhtran811@gmail.com>
2022-12-02 20:35:54 -0700

    User story 9 - Parent Index Link, added parent links to all players views with associated tests

--- spec/features/players/show_spec.rb
+++ spec/features/players/show_spec.rb
@@ -50,7 +50,22 @@ RSpec.describe 'Player Show' do
       visit "/player_table_name/#{player_1.id}"
 
       click_link('Player Index')
+
       expect(current_path).to eq('/player_table_name')
+    end
+
+# As a visitor
+# When I visit any page on the site
+# Then I see a link at the top of the page that takes me to the Parent Index
+    it 'I can see a link at the top of the page that takes me to the teams index' do
+      team = Team.create!(name: 'Spain', rank: 7, qualified: true)
+      player_1 = Player.create!(name: 'Koke', age: 30, old_enough: true, team_id: team.id)
+    
+      visit "/player_table_name/#{player_1.id}"
+
+      click_link('Team Index')
+
+      expect(current_path).to eq('/teams')
     end
   end
 end
