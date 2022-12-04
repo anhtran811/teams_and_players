543299a81c9ad133af5f3a7469be8d2b3aa90157
Anhnhi Tran <anhtran811@gmail.com>
2022-12-04 09:56:21 -0700

    Refactor create team players

--- app/controllers/team_players_controller.rb
+++ app/controllers/team_players_controller.rb
@@ -9,12 +9,17 @@ class TeamPlayersController < ApplicationController
   end
 
   def create
-    player = Player.create(
-      team_id: params[:team_id],
-      name: params[:name],
-      age: params[:age],
-      old_enough: params[:old_enough])
+    player = Player.create(team_players_params)
+    #   player = Player.create(
+    #   team_id: params[:team_id],
+    #   name: params[:name],
+    #   age: params[:age],
+    #   old_enough: params[:old_enough])
 
     redirect_to "/teams/#{player.team_id}/player_table_name"
+  end
+
+  def team_players_params
+    params.permit(:team_id, :name, :age, :old_enough)
   end
 end
