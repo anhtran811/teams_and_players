c21c76369f50726ef3d1188a0e9571c909e5574b
Anhnhi Tran <anhtran811@gmail.com>
2022-12-04 14:29:04 -0700

    User story 16 - Sort Teams Player in Alphabetical Order by name test for link is passing

--- spec/features/teams/players/index_spec.rb
+++ spec/features/teams/players/index_spec.rb
@@ -59,5 +59,22 @@ RSpec.describe 'Teams Players Index' do
 
       expect(current_path).to eq('/teams')
     end
+
+# As a visitor
+# When I visit the Parent's children Index Page
+# Then I see a link to sort children in alphabetical order
+# When I click on the link
+# I'm taken back to the Parent's children Index Page where I see all of the parent's children in alphabetical order
+    it 'can see a link to sort the players alphabetically' do
+      team = Team.create!(name: 'Spain', rank: 7, qualified: true)
+      koke = team.players.create!(name: 'Koke', age: 30, old_enough: true)
+      gave = team.players.create!(name: 'Gavi', age: 18, old_enough: false)
+      torres = team.players.create!(name: 'Torres', age: 35, old_enough: true)
+
+      visit "/teams/#{team.id}/players"
+      click_link ("#{team.name} players listed alphabetically")
+
+      expect(current_path).to eq("/teams/#{team.id}/players")
+    end
   end
 end
