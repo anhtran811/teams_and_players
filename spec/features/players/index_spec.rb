f8e46dfcc44f7967a280e5fd31f614239c223984
Anhnhi Tran <anhtran811@gmail.com>
2022-12-04 17:45:22 -0700

    User story 18 - Child update from Childs Index Page complete with associated filse and tests passing

--- spec/features/players/index_spec.rb
+++ spec/features/players/index_spec.rb
@@ -68,6 +68,28 @@ RSpec.describe 'Player Index' do
         expect(page).to have_content(torres.age)
         expect(page).to have_content(torres.old_enough)
       end
+
+# As a visitor
+# When I visit the `child_table_name` index page or a parent `child_table_name` index page
+# Next to every child, I see a link to edit that child's info
+# When I click the link
+# I should be taken to that `child_table_name` edit page where I can update its information just like in User Story 14
+      it 'can see a link next to every player to edit the player' do
+        spain = Team.create!(name: 'Spain', rank: 7, qualified: true)
+        koke = spain.players.create!(name: 'Koke', age: 30, old_enough: true)
+        torres = spain.players.create!(name: 'Torres', age: 35, old_enough: true)
+        
+        visit '/players'
+        click_link("Edit: #{koke.name}")
+
+        expect(current_path).to eq("/players/#{koke.id}/edit")
+        
+        
+        visit '/players'
+        click_link("Edit: #{torres.name}")
+
+        expect(current_path).to eq("/players/#{torres.id}/edit")
+      end
     end
   end
 end
