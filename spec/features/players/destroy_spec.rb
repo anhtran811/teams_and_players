71ed1f239cd6b8944f512e1fc120c91d7129bec9
Anhnhi Tran <anhtran811@gmail.com>
2022-12-06 13:39:10 -0700

    User story 23 - child delete from childs index page complete with passing tests

--- spec/features/players/destroy_spec.rb
+++ spec/features/players/destroy_spec.rb
@@ -22,5 +22,24 @@ RSpec.describe 'Destroying a player' do
         expect(page).to_not have_content('Silva')
       end
     end
+
+# As a visitor
+# When I visit the `child_table_name` index page or a parent `child_table_name` index page
+# Next to every child, I see a link to delete that child
+# When I click the link
+# I should be taken to the `child_table_name` index page where I no longer see that child
+    describe 'When I visit a players index page' do
+      it 'can delete a player' do
+        portugal = Team.create!(name: 'Portugal', rank: 9, qualified: true)
+        silva = portugal.players.create!(name: 'Silva', age: 19, old_enough: true)
+
+        visit '/players'
+        click_button("Delete: #{silva.name}")
+
+        expect(current_path).to eq('/players')
+        expect(page).to_not have_content('Silva')
+      end
+    end
+    
   end
 end
