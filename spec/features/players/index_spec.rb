c3a03bd562924d0489dbda38de226c45349d38cb
Anhnhi Tran <anhtran811@gmail.com>
2022-12-06 16:08:12 -0700

    Removed all commented out lines in players spec files

--- spec/features/players/index_spec.rb
+++ spec/features/players/index_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
-# As a visitor
-# When I visit '/child_table_name'
-# Then I see each Child in the system including the Child's attributes
-# (data from each column that is on the child table)
-
 RSpec.describe 'Player Index' do
   describe 'As a visitor' do
     describe "When I visit my players index page '/players" do
@@ -19,10 +14,7 @@ RSpec.describe 'Player Index' do
         expect(page).to have_content(true)
       end
 
-# As a visitor
-# When I visit the child index
-# Then I only see records where the boolean column is `true`
-      it 'I will only see the records where the boolean column is true' do
+      it 'I will only see the records where the old enough column is true' do
         spain = Team.create!(name: 'Spain', rank: 7, qualified: true)
         koke = spain.players.create!(name: 'Koke', age: 30, old_enough: true)
         gavi = spain.players.create!(name: 'Gavi', age: 18, old_enough: false)
@@ -41,11 +33,6 @@ RSpec.describe 'Player Index' do
         expect(page).to have_content(torres.old_enough)
       end
 
-# As a visitor
-# When I visit the `child_table_name` index page or a parent `child_table_name` index page
-# Next to every child, I see a link to edit that child's info
-# When I click the link
-# I should be taken to that `child_table_name` edit page where I can update its information just like in User Story 14
       it 'can see a link next to every player to edit the player' do
         spain = Team.create!(name: 'Spain', rank: 7, qualified: true)
         koke = spain.players.create!(name: 'Koke', age: 30, old_enough: true)
